6d8681b6fe56e7739d7a2197b57f3c7ec84b1a0f
unknown <Nuwan@NUWAN-PC.bquzone.lk>
2014-04-23 10:45:05 +0530

    first commit

---
+++ ci_multiple_checkbox_check.php
@@ -0,0 +1,45 @@
+<?php 
+
+/**
+ * @author	Nuwan Wijethilaka
+ * @link	http://www.linkedin.com/in/nuwanlinkedin
+ * @email	nu1.rock@gmail.com
+ * @file	checkbox.php
+ */
+ 
+/**
+ * checkbox value fixer. If a checkbox is not checked, sets it's value
+ * to zero.
+ * @param mixed $name the checkbox form field name. Can be passed as a
+ * string or as an array.
+ * @return void
+ */
+ 
+if ( ! function_exists('multiple_checkbox_check'))
+{
+	function multiple_checkbox_check($id, $selected, $extra)
+	{
+		if(array_key_exists ($id ,$selected))
+		{
+			return 'checked="checked"';
+			
+		}
+	}
+}
+
+if ( ! function_exists('form_multiple_checkbox'))
+{
+	function form_multiple_checkbox($name = '', $all = array(), $selected = array(), $extra = '')
+	{
+		foreach ($all as $optgroup_key => $optgroup_val)
+		{ 
+		 		
+			echo '<input type="checkbox" name="'.$name.'" value="'.$optgroup_key.'" '.multiple_checkbox_check($optgroup_key,$selected,$extra).' >'; 
+			echo '&nbsp;&nbsp';
+			echo (string) $optgroup_val."<br>";	 	
+				 
+						
+		}
+		 
+	}
+}
